da4abd4d0b987bcdab08fd27f863589f6fb9d58c
Khaoulaad <ouajaa.khaoula@gmail.com>
2023-08-23 15:16:09 -0400

    0

--- execute.c
+++ execute.c
@@ -11,7 +11,8 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall},
-				{"pint", f_pint},
+				{"pint", f_pint}, {"nop", f_nop},
+				{"pop", f_pop},
 				{NULL, NULL}
 				};
 	unsigned int a = 0;
